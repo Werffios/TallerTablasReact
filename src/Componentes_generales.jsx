dd6507ba457c3d65eed868fc5f1c5b9a11f838b8
Your Name <nasuarezro@unal.edu.co>
2022-05-18 11:43:04 -0500

    Nav Bar completa

--- src/Componentes_generales.jsx
+++ src/Componentes_generales.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {Formulario} from './Formulario';
 import {Tabla} from "./Tabla";
+import {NavBar} from "./NavBar";
 
 
 export function Aplicacion(){
     return(
         <React.Fragment>
+            <NavBar/>
             <Tabla/>
             <Formulario/>
         </React.Fragment>
